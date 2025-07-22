e6486767d6e3a9e5e3246c7612a3216a6547dda7
appaddbiz <appaddbiz@gmail.com>
2025-07-22 22:31:30 +0530

    page-url:update

--- src/components/landing-page/WPEmbed.jsx
+++ src/components/landing-page/WPEmbed.jsx
@@ -1,15 +1,13 @@
-
-
 const WPEmbed = () => {
   return (
     <div style={{ height: "100vh", width: "100%", overflow: "hidden" }}>
       <iframe
-        src="https://clinic.leodental.co.in"
+        src="https://clinic.leodental.co.in/best-dental-clinic-in-whitefield"
         title="Best Dental Clinic in Whitefield"
         style={{
           width: "100%",
           height: "100%",
-          border: "none"
+          border: "none",
         }}
         allowFullScreen
       />
