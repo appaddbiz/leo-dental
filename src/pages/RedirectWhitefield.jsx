93846afbf9373dba54b52498a07765f8633b8912
appaddbiz <appaddbiz@gmail.com>
2025-07-22 20:01:40 +0530

    page-url:update

--- src/pages/RedirectWhitefield.jsx
+++ src/pages/RedirectWhitefield.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 export default function RedirectWhitefield() {
   useEffect(() => {
     window.location.href =
-      "https://clinic.leodental.co.in/best-dental-clinic-in-whitefield/";
+      "https://www.leodental.co.in/best-dental-clinic-in-whitefield/";
   }, []);
 
   return <p>Redirecting to our Whitefield dental clinic page...</p>;
