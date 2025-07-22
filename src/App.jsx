85f08e56fe4733fc3968c7710fad08303229bfbc
appaddbiz <appaddbiz@gmail.com>
2025-07-22 23:02:37 +0530

    page-url:update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useLocation } from "react-router-dom";
+import Footer from "./components/Footer";
 import Layout from "./Layout/Layout";
 
 function App() {
-  return <Layout />;
+  const location = useLocation();
+  const hideFooterRoutes = [
+    "/best-dental-clinic-in-hsr-layout",
+    "/best-dental-clinic-in-nellore",
+    "/best-dental-clinic-in-tirupathi",
+    "/best-dental-clinic-in-whitefield",
+  ];
+
+  return (
+    <>
+      <Layout />
+      {!hideFooterRoutes.includes(location.pathname) && <Footer />}
+    </>
+  );
 }
 
 export default App;
