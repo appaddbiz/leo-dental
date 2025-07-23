4b91dc5c97c169b43611fd8ed2fe44fd107bb3f7
appaddbiz <appaddbiz@gmail.com>
2025-07-23 19:54:20 +0530

    page-url:update

--- src/components/landing-page/Whitefield.jsx
+++ src/components/landing-page/Whitefield.jsx
@@ -1,12 +1,10 @@
 const Whitefield = () => {
   return (
-    <div
-      style={{ height: "100vh", width: "100%", overflow: "hidden" }}
-      className="hide-scrollbar"
-    >
+    <div style={{ height: "100vh", width: "100%", overflow: "hidden" }}>
       <iframe
         src="https://clinic.leodental.co.in/best-dental-clinic-in-whitefield"
         title="Best Dental Clinic in Whitefield"
+        className="hide-scrollbar"
         style={{
           width: "100%",
           height: "100%",
