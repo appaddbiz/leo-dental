0e434d2ca8725de8a8d3741447659e28dd26557a
appaddbiz <appaddbiz@gmail.com>
2025-07-23 11:11:57 +0530

    page-url:update

--- src/pages/landing-page/best-dental-clinic-in-whitefield.jsx
+++ src/pages/landing-page/best-dental-clinic-in-whitefield.jsx
@@ -1,15 +1,13 @@
+import { useEffect } from "react";
 import Whitefield from "../../components/landing-page/Whitefield";
 
 function BestDentalClinicInWhitefield() {
+  useEffect(() => {
+    document.body.classList.add("hide-scrollbar");
+    return () => document.body.classList.remove("hide-scrollbar");
+  }, []);
   return (
-    <div
-      style={{
-        overflow: "hidden",
-        scrollbarWidth: "none", // Firefox
-        msOverflowStyle: "none", // IE and Edge
-      }}
-      className="no-scrollbar"
-    >
+    <div>
       <Whitefield />
     </div>
   );
