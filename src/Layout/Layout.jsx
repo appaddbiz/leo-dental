195202a66000452f0e825ab91c80b07734b8d6e4
appaddbiz <appaddbiz@gmail.com>
2025-07-23 10:49:52 +0530

    page-url:update

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -73,7 +73,7 @@ const Layout = () => {
           />
         </Routes>
       </main>
-      <ContactUs />
+      {/* <ContactUs /> */}
       {!hideFooterRoutes.includes(normalizedPath) && <Footer />}
     </div>
   );
