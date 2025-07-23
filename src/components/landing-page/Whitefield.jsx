80953a40c203c0ff5baf03052cd5f81355aa1b35
appaddbiz <appaddbiz@gmail.com>
2025-07-23 10:58:30 +0530

    page-url:update

--- src/components/landing-page/Whitefield.jsx
+++ src/components/landing-page/Whitefield.jsx
@@ -1,19 +1,25 @@
 const Whitefield = () => {
   return (
-    // <div style={{ height: "100vh", width: "100%", overflow: "hidden" }}>
-    <iframe
-      src="https://clinic.leodental.co.in/best-dental-clinic-in-whitefield"
-      title="Best Dental Clinic in Whitefield"
+    <div
       style={{
+        height: "100vh",
         width: "100%",
-        height: "100%",
-        border: "none",
         overflow: "hidden",
         scrollbars: "none",
       }}
-      allowFullScreen
-    />
-    // </div>
+    >
+      <iframe
+        src="https://clinic.leodental.co.in/best-dental-clinic-in-whitefield"
+        title="Best Dental Clinic in Whitefield"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+          overflow: "hidden",
+        }}
+        allowFullScreen
+      />
+    </div>
   );
 };
 
