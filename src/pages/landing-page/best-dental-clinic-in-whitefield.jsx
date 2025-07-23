8c825cc16ab4772bcb2f28705b32408c656597dd
appaddbiz <appaddbiz@gmail.com>
2025-07-23 11:02:35 +0530

    page-url:update

--- src/pages/landing-page/best-dental-clinic-in-whitefield.jsx
+++ src/pages/landing-page/best-dental-clinic-in-whitefield.jsx
@@ -3,7 +3,12 @@ import Whitefield from "../../components/landing-page/Whitefield";
 function BestDentalClinicInWhitefield() {
   return (
     <div
-      style={{ overflowX: "hidden", overflowY: "hidden", scrollbars: "none" }}
+      style={{
+        overflow: "hidden",
+        scrollbarWidth: "none", // Firefox
+        msOverflowStyle: "none", // IE and Edge
+      }}
+      className="no-scrollbar"
     >
       <Whitefield />
     </div>
